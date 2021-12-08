22db02d9e8491973548f94de0b1acd15a90a3661
vahagn <vahagn.1984@mail.ru>
2021-12-08 12:58:44 +0400

    task Type Conversion

---
+++ whatOutput.js
@@ -0,0 +1,16 @@
+"use strict";
+
+"" + 1 + 0; //output 10
+"" - 1 + 0; //output -1
+true + false; //output 1
+6 / "3"; //output 2
+"2" * "3"; //output 6
+4 + 5 + "px"; //output 9px
+"$" + 4 + 5; //output $45
+"4" - 2; //output 2
+"4px" - 2; //output NaN
+7 / 0; //output infinity
+"-9" + 5; //output -95
+"-9" - 5; //output -14
+null + 1; //output 1
+undefined + 1; //output NaN
